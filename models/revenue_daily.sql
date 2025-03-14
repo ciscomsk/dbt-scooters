f193d6e3cfd410933b33418e67923c74728666a1
ciscomsk <kuznetzoff@gmail.com>
2025-03-14 17:40:30 +0300

    Mission_6_3

---
+++ models/revenue_daily.sql
@@ -0,0 +1,12 @@
+SELECT
+    date,
+    SUM(price_rub) AS revenue_rub,
+    NOW() AT TIME ZONE 'UTC' AS updated_at
+FROM {{ ref("trips_prep") }}
+{% if is_incremental() %}
+    WHERE date >= (SELECT MAX(date) - INTERVAL '2' DAY FROM {{ this }})
+{% endif %}
+GROUP BY
+    date,
+    updated_at
+ORDER BY date
