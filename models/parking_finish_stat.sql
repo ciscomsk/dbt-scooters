4baa3e5c94448ac11e8a1a5a417fa8ba61630bfc
ciscomsk <kuznetzoff@gmail.com>
2025-03-23 20:07:57 +0300

    Mission_8_3

--- models/parking_finish_stat.sql
+++ models/parking_finish_stat.sql
@@ -1,7 +1,1 @@
-SELECT
-    st_transform(hex.geom, 4326) as geom,
-    COUNT(*) as trips
-FROM {{ ref("trips_geom") }} as t
-CROSS JOIN st_hexagongrid(10, st_transform(t.finish_point, 3587)) as hex
-WHERE st_intersects(st_transform(t.finish_point, 3587), hex.geom)
-GROUP BY geom
+{{ trips_geom_stat(ref("trips_geom"), geom_col="finish_point", grid=10) }}
